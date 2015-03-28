32098aaf76340017b119d3c18ac49ff4610d212b
Avery Lamp <averylamp@gmail.com>
2015-03-28 18:09:02 -0400

    Added Categories and NSManagedSubclass

---
+++ ActionTap/Pattern+Pattern_Functions.h
@@ -0,0 +1,14 @@
+//
+//  Pattern+Pattern_Functions.h
+//  ActionTap
+//
+//  Created by Avery Lamp on 3/28/15.
+//  Copyright (c) 2015 Sebastian Cain. All rights reserved.
+//
+
+#import "Pattern.h"
+
+@interface Pattern (Pattern_Functions)
++(NSNumber *) comparePattern:(Pattern *)firstPattern with: (Pattern*)secondPattern;
+
+@end
---
+++ ActionTap/Pattern.m
@@ -0,0 +1,18 @@
+//
+//  Pattern.m
+//  ActionTap
+//
+//  Created by Avery Lamp on 3/28/15.
+//  Copyright (c) 2015 Sebastian Cain. All rights reserved.
+//
+
+#import "Pattern.h"
+
+
+@implementation Pattern
+
+@dynamic allTaps;
+@dynamic name;
+@dynamic url;
+
+@end

e269d1b6348b9d520342e61815d5e159e5d37aed
sebastianCain <sebi.cain@yahoo.com>
2015-03-28 15:54:34 -0400

    Initial Commit

---
+++ ActionTap/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  ActionTap
+//
+//  Created by Sebastian Cain on 3/28/15.
+//  Copyright (c) 2015 Sebastian Cain. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ ActionTap/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  ActionTap
+//
+//  Created by Sebastian Cain on 3/28/15.
+//  Copyright (c) 2015 Sebastian Cain. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+	[super viewDidLoad];
+	// Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+	[super didReceiveMemoryWarning];
+	// Dispose of any resources that can be recreated.
+}
+
+@end

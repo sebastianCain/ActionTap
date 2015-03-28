5009390ddbf815785798b119acc51a8b362cb126
Avery Lamp <averylamp@gmail.com>
2015-03-28 18:03:11 -0400

    Recorder Class to make record  & CoreData

---
+++ ActionTap/Recorder.h
@@ -0,0 +1,13 @@
+//
+//  Recorder.h
+//  ActionTap
+//
+//  Created by Avery Lamp on 3/28/15.
+//  Copyright (c) 2015 Sebastian Cain. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface Recorder : UIViewController
+
+@end
---
+++ ActionTap/Recorder.m
@@ -0,0 +1,37 @@
+//
+//  Recorder.m
+//  ActionTap
+//
+//  Created by Avery Lamp on 3/28/15.
+//  Copyright (c) 2015 Sebastian Cain. All rights reserved.
+//
+
+#import "Recorder.h"
+
+@interface Recorder ()
+
+@end
+
+@implementation Recorder
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end

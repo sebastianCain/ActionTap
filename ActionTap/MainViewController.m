c24bc0e5c8a27916f89d39f07aa26ddbfd8172d7
Avery Lamp <averylamp@gmail.com>
2015-03-29 09:43:06 -0400

    Fixed Bug

--- ActionTap/MainViewController.m
+++ ActionTap/MainViewController.m
@@ -30,6 +30,7 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     [self refreshAllPatterns];
+    NSLog(@"second");
     [self refreshAllPatternsForTV];
     
 	[self.view setBackgroundColor:[UIColor colorWithRed:40/255.0 green:40/255.0 blue:40/255.0 alpha:1.0]];
@@ -250,6 +251,7 @@
     }
     if (!backgroundPattern) {
         backgroundPattern =[NSEntityDescription insertNewObjectForEntityForName:@"Pattern" inManagedObjectContext:context];
+        backgroundPattern.name= @"BackgroundPattern";
     }
     self.backgroudPattern = backgroundPattern;
     
@@ -377,6 +379,7 @@
 }
 
 -(void)refreshActionPage{
+    NSLog(@"refreshActionPage");
     self.textField.text= self.pickedPattern.name;
 }
 
@@ -464,14 +467,21 @@
 }
 
 -(void)refreshAllPatterns{
+    NSLog(@"refresh patterns");
     NSManagedObjectContext *context = [DataAccess context];
     NSFetchRequest *request = [NSFetchRequest fetchRequestWithEntityName:@"Pattern"];
-    NSArray *tempAll = [context executeFetchRequest:request error:nil];
+    NSError *error;
+    NSArray *tempAll = [context executeFetchRequest:request error:&error];
     self.allPatterns = [[NSMutableDictionary alloc]init];
     for (Pattern *p in tempAll) {
+        NSLog(@"name - %@",p.name);
+        if (!p.name) {
+            [context deleteObject:p];
+            continue;
+        }
         [self.allPatterns  setValue:p forKey:p.name];
     }
-    
+    [[DataAccess sharedInstance]saveContext];
     
 }
 
@@ -485,7 +495,7 @@
 }
 
 -(void)refreshLines{
-    
+    NSLog(@"refresh lines");
     NSData *test = self.pickedPattern.allTaps;
     if (test ==[NSNull null]) {
         NSLog(@"First Null");
@@ -563,13 +573,10 @@
 
 -(void)startRecordingInBackground{
     
-    if(self.backgroundRecorder.isRecording == NO)
+    if(self.backgroundRecorder.isRecording == NO &&self.pageControl.currentPage ==0)
     {
         self.backgroundRecorder.delegate = self;
         [self.backgroundRecorder startNewPatternWithPattern:self.backgroudPattern isBackground:YES];
-    } else
-    {
-        [self.backgroundRecorder stopRecording];
     }
 }
 
@@ -636,8 +643,12 @@
     NSFetchRequest *request = [NSFetchRequest fetchRequestWithEntityName:@"Pattern"];
     NSSortDescriptor *sortDescriptor = [[NSSortDescriptor alloc]initWithKey:@"name" ascending:YES];
     [request setSortDescriptors:[NSArray arrayWithObject:sortDescriptor]];
-    self.allPatternsForTV = [context executeFetchRequest:request error:nil];
-    
+    self.allPatternsForTV = [[context executeFetchRequest:request error:nil] mutableCopy];
+    for (Pattern *p in self.allPatternsForTV) {
+        if ([p.name isEqualToString:@"BackgroundPattern"]) {
+            [self.allPatternsForTV removeObject:p];
+        }
+    }
 }
 
 -(void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender{
